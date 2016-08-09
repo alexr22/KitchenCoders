a9f0cb522f896be52622240e17884368492afe07
LouiseKMiller <louise.austex@gmail.com>
2016-08-09 12:08:06 -0500

    changes from Monday night at class

--- getRecipes.js
+++ getRecipes.js
@@ -1,5 +1,7 @@
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 var request = require('request');
+var fs = require('fs');
+
 
     var queryURL = "";
     var numResults = 0;
@@ -8,7 +10,7 @@ var request = require('request');
   // BigOven key
     var authKey ="b51799f689f89bdc92b57ec5e8f5157e";
     var queryURLBase = "http://food2fork.com/api/search?key=" + authKey + "&q=";
- 
+
  // Then run a request to the OMDB API with the movie specified
 request(queryURLBase + "chicken", function (error, response, body) {
 
@@ -16,7 +18,18 @@ request(queryURLBase + "chicken", function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 
 		// Parse the body of the site and recover just the imdbRating
-		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
-		console.log(body);
+		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+		fs.writeFile("recipes.txt", body, function(err) {
+
+			// If the code experiences any errors it will log the error to the console.
+    		if(err) {
+        		return console.log(err);
+    		}
+
+    		// Otherwise, it will print: "movies.txt was updated!"
+    		console.log("recipes.txt was updated!");
+		});
 	}
 });
+
