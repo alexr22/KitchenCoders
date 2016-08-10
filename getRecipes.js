458fda0963a4976a5b42c221336d828f8afb3bfd
LouiseKMiller <louise@txmiller.com>
2016-08-10 02:39:18 -0500

    getRecipe.js is working - can populate all three tables

--- getRecipes.js
+++ getRecipes.js
@@ -29,15 +29,98 @@ var recipeID ="";
 var searchTerm = "";
 var recipeIngredients = [];
 
-function enterRecipe(newRecipe){
-    // We run this query so that we can drop our tables even though they have foreign keys
-    seqConnection.query('SET FOREIGN_KEY_CHECKS = 0')
 
-    // reach into our models object, and create each table based on the associated model.
-    // note: force:true drops the table if it already exists
-    .then(function(){
-        return seqConnection.sync()
-    })
+// ON A PER RECIPE BASIS, ADD INGREDIENTS TO THE DATABASE IF THEY ARE NEW
+// MUST LET EACH findOrCreate COMPLETE BEFORE INITIATING THE NEXT ONE FOR
+// THE NEXT INGREDIENT.  OTHERWISE, YOU MAY NOT FIND A PRIOR INGREDIENT
+// THAT IS STILL IN PROCESS.
+function addToTable(ingredients, newRecipe, recipeIngredients){
+  var i = 0;
+  function forloop(){
+    if(i<ingredients.length){
+        models.Ingredient.findOrCreate({where: {spoonID: ingredients[i].spoonID}, defaults: {name: ingredients[i].name, category: ingredients[i].category}})
+        .then(function(){
+            i++;
+            forloop();
+        });
+    }
+    else{
+        createRecipe(newRecipe, recipeIngredients);
+    }
+  }
+  forloop();
+}
+
+
+function createRecipe(newRecipe, recipeIngredients){
+    return models.Recipe.create(
+        {title: newRecipe.title,
+        image: newRecipe.image,
+        vegetarian: newRecipe.vegetarian,
+        vegan: newRecipe.vegan,
+        glutenFree: newRecipe.glutenFree,
+        servings: newRecipe.servings,
+        preparationMinutes: newRecipe.preparationMinutes,
+        cookingMinutes: newRecipe.cookingMinutes,
+        sourceUrl: newRecipe.sourceUrl
+        })
+   .then(function(recipe){
+        var ingredientNames =[];
+        for (var x=0; x<recipeIngredients.length; x++){
+            ingredientNames.push(recipeIngredients[x].name);
+        }
+       return models.Ingredient.findAll({where: {name: ingredientNames}})
+            .then(function(ingredients){recipe.addIngredients(ingredients);
+       })
+   })
+}
+
+
+function processAllRecipes(recipes){
+    var j=0;
+    function outerloop(){
+        if (j < recipes.length){
+
+// FOR EACH RECIPE IN recipeResults array, you need to do two searches
+            recipeID = recipes[j].id;
+
+
+    //first you need to search by recipeID and find the recipe information
+            unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recipeID + "/information?includeNutrition=false")
+            .header("X-Mashape-Key", "1pb1awVrWQmsh5cGX7uf2JqubVkIp1ibFl8jsnOPSRyTSkfXtR")
+            .end(function (result) {
+
+                oneRecipeData = {
+                    title: result.body.title,
+                    image: result.body.image,
+                    vegetarian: result.body.vegetarian,
+                    vegan: result.body.vegan,
+                    glutenFree: result.body.glutenFree,
+                    servings: result.body.servings,
+                    preparationMinutes: result.body.preparationMinutes,
+                    cookingMinutes: result.body.cookingMinutes,
+                    sourceUrl: result.body.sourceUrl,
+                    extendedIngredients: result.body.extendedIngredients
+
+                };
+            processOneRecipe(oneRecipeData);
+            j++;
+            outerloop();
+            });
+        } else {
+            console.log("outer loop done");
+        }
+
+    }
+    outerloop();
+}
+// ADD A RECIPE
+
+function processOneRecipe(newRecipe){
+    // We run this query so that we can drop our tables even though they have foreign keys
+//    seqConnection.query('SET FOREIGN_KEY_CHECKS = 0')
+
+    seqConnection.sync()
     // enter new ingredients
     .then(function(){
 
@@ -52,104 +135,42 @@ function enterRecipe(newRecipe){
                 category: extendedIngredients[i].aisle
             };
             var recipeIngredient = {
-                name: extendedIngredients[i].name,
-                amount: extendedIngredients[i].amount,
-                unit: extendedIngredients[i].unit
+                name: extendedIngredients[i].name
+//                amount: extendedIngredients[i].amount,
+//                unit: extendedIngredients[i].unit
             };
-        	recipeIngredients.push(recipeIngredient);
         	newIngredients.push(newIngredient);
-        	models.Ingredient.findOrCreate({where: {spoonID: extendedIngredients[i].id}, defaults: {name: extendedIngredients[i].name, category: extendedIngredients[i].aisle}})
+            recipeIngredients.push(recipeIngredient);
         }
-       })
-    //    console.log("newIngredients here:", newIngredients);
-    //    console.log("recipeIngredients here:", recipeIngredients);
-    //    return models.Ingredient.bulkCreate(newIngredients, {ignoreDuplicates: true})
 
-    // then enter new recipe
-	    .then(function(){
-	     return models.Recipe.create(
-	        {title: newRecipe.title,
-	        image: newRecipe.image,
-	        vegetarian: newRecipe.vegetarian,
-	        vegan: newRecipe.vegan,
-	        glutenFree: newRecipe.glutenFree,
-	        servings: newRecipe.servings,
-	        preparationMinutes: newRecipe.preparationMinutes,
-	        cookingMinutes: newRecipe.cookingMinutes,
-	        sourceUrl: newRecipe.sourceUrl
-	    	})
-	 	})
-		//        .then(function(recipe){
-		//            return models.Ingredient.findAll({where: {name: [recipeIngredients]}})
-		//            .then(function(ingredients){recipe.addIngredients(ingredients);
-		//            })
-		//        })
-
-
-
+        addToTable(newIngredients, newRecipe, recipeIngredients);
+    })
 }
 
-
-
+//========================================================================
+//          THIS IS WHERE THE ACTION STARTS
+//========================================================================
+//
+// First we run this query so that we can drop our tables even though they have foreign keys
+seqConnection.query('SET FOREIGN_KEY_CHECKS = 0')
 
 // SEARCH FOR 10 RECIPES - RIGHT NOW THEY ARE FOR BURGER RECIPES
-unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=burger&type=main+course")
+unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=10&offset=0&query=ham&type=main+course")
 .header("X-Mashape-Key", "1pb1awVrWQmsh5cGX7uf2JqubVkIp1ibFl8jsnOPSRyTSkfXtR")
 .end(function (result) {
 
 // STORE RESULTS IN recipeResults array
     recipeSearchResults = result.body.results;
 
-// FOR EACH RECIPE IN recipeResults array, you need to do two searches
-    for (var i=0; i<recipeSearchResults.length; i++) {
-        recipeID = recipeSearchResults[i].id;
+    processAllRecipes(recipeSearchResults);
+
+});
 
 
-    //first you need to search by recipeID and find the recipe information
-        unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recipeID + "/information?includeNutrition=false")
-        .header("X-Mashape-Key", "1pb1awVrWQmsh5cGX7uf2JqubVkIp1ibFl8jsnOPSRyTSkfXtR")
-        .end(function (result) {
-
-            oneRecipeData = {
-                title: result.body.title,
-                image: result.body.image,
-                vegetarian: result.body.vegetarian,
-                vegan: result.body.vegan,
-                glutenFree: result.body.glutenFree,
-                servings: result.body.servings,
-                preparationMinutes: result.body.preparationMinutes,
-                cookingMinutes: result.body.cookingMinutes,
-                sourceUrl: result.body.sourceUrl,
-                extendedIngredients: result.body.extendedIngredients
-
-            };
-            enterRecipe(oneRecipeData);
-        });
-    }
-
-})
-
-
-
-
-
-
-// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/262682/information?includeNutrition=false")
+//  this section is for the instructions.  Still need to work on this
+// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/262682/analyzedInstructions?stepBreakdown=false")
 // .header("X-Mashape-Key", "1pb1awVrWQmsh5cGX7uf2JqubVkIp1ibFl8jsnOPSRyTSkfXtR")
+// .header("Accept", "application/json")
 // .end(function (result) {
-
-//         fs.writeFile("recipes_detailed.txt", JSON.stringify(result.body), function(err) {
-//             if(err) {
-//                 return console.log(err);
-//             }
-//             // Otherwise, it will print: "movies.txt was updated!"
-//             console.log("recipes_detailed.txt was updated!");
-//         });
-
-
-// console.log(result.status, result.headers, result.body);
+//  console.log('****************************', result.body);
 // });
-
-
-
-// }
