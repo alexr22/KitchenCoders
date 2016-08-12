8a628021aae9ae5f9cf777a982639404217426a3
LouiseKMiller <louise@txmiller.com>
2016-08-12 11:16:08 -0500

    updated all handlebars pages

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -92,6 +92,26 @@ router.get('/recipe', function (req, res) {
 	});
 });
 
+
+// GET REQUEST TO URI - /findRecipe  
+// user presented with page where she can
+// query database for matching recipes
+// add addition limitation that all ingredients must be inStock
+//
+router.get('/findRecipe', function (req, res) {
+	res.render('findRecipe');
+	
+});
+
+
+// GET REQUEST TO URI - /addRecipe  
+// user presented with page where she can
+// query database for matching recipes
+// add addition limitation that all ingredients must be inStock
+//
+router.get('/addRecipe', function (req, res) {
+	res.render('addRecipe');
+});
 //
 // POST REQUEST TO URI  - /RECIPE/ADD
 // receives new recipe entered by user
@@ -134,5 +154,106 @@ router.post('/admin/add', function (req, res) {
 	res.redirect('/home');
 
 });
+
+//******************************************************
+//  ROUTES FOR CONTACTUS
+//******************************************************
+//
+// GET REQUEST TO URI - /RECIPE  (*** should change to /RECIPE/RESULTS ??)
+// user has entered filtering information, which is used below
+// to query database for matching recipes
+// add addition limitation that all ingredients must be inStock
+//
+router.get('/contactUs', function (req, res) {
+
+	res.render('contactUs');
+
+});
+
+
+//******************************************************
+//  ROUTE FOR HOME
+//******************************************************
+// THIS IS THE HOME PAGE
+// From this page, user can decide what to do
+// User can
+//    (1) go to the ingredients page
+//    (2) go to the findRecipes page
+//	  (3) go to the addRecipes page
+//    (4) go to the preferences page
+// router.get('/home', function (req, res) {
+// 	res.sendFile(path.join(__dirname + '/../public/home.html'));
+// });
+
+
+//******************************************************
+//  ROUTE FOR INGREDIENTS
+//******************************************************
+// HERE IS WHERE WE SERVE UP THE PUBLIC STATIC HTML PAGE FOR INGREDIENTS
+// From this page, user can
+//    (1) add ingredients,
+//    (2) change status of ingredient to inStock or not inStock, or
+//    (3) make a change to an ingredient already in the database (other than inStock)
+//    (4) delete an ingredient
+//    MODEL THIS AFTER OUR BURGERS HOMEWORK
+// router.get('/ingredient', function (req, res) {
+// 	res.sendFile(path.join(__dirname + '/../public/ingredient.html'));
+// });
+
+//******************************************************
+//  ROUTE FOR FIND RECIPE
+//******************************************************
+// HERE IS WHERE WE SERVE UP THE PUBLIC STATIC HTML PAGE FOR FIND RECIPES
+// From this page, user can
+//    (1) enter filtering information and initiate a search for recipes,
+//    (2) ???
+//    (3) ???
+//
+// *******note - remember to redefine the /recipe route in the api_routes file
+// ******* to somethinglike /recipe/results
+//router.get('/recipe', function (req, res) {
+//	res.sendFile(path.join(__dirname + '/../public/findRecipe.html'));
+//});
+
+//
+//******************************************************
+//  ROUTE FOR ADDING A RECIPE
+//******************************************************
+// HERE IS WHERE WE SERVE UP THE PUBLIC STATIC HTML PAGE FOR ADDING A RECIPE
+// From this page, user can
+//    (1) ENTER INFORMATION FOR A RECIPE AND ADD IT TO THE DATABASE
+//
+// *******note - remember to redefine the /recipe route in the api_routes file
+// ******* to somethinglike /recipe/results
+//router.get('/recipe', function (req, res) {
+//	res.sendFile(path.join(__dirname + '/../public/addRecipe.html'));
+//});
+//
+//
+//******************************************************
+//  ROUTE FOR PREFERENCES
+//******************************************************
+// HERE IS WHERE WE SERVE UP THE PUBLIC STATIC HTML PAGE FOR PREFERENCES
+// From this page, user can
+//    (1) enter information regarding the user's preferences, like food allergies and other dietary restrictins.  Other???
+//    (2) ???
+//    (3) ???
+//
+//router.get('/preference', function (req, res) {
+//	res.sendFile(path.join(__dirname + '/../public/preferences.html'));
+//});
+
+//******************************************************
+//  ROUTE FOR SIGN-IN
+//******************************************************
+// HERE IS WHERE WE SERVE UP THE PUBLIC STATIC HTML PAGE FOR SIGN-IN
+// From this page, user can
+//    (1) enter user information,
+//    (2) indicate whether user is a first time user
+//    (3) ???
+//
+//router.get('/login', function (req, res) {
+//	res.sendFile(path.join(__dirname + '/../public/login.html'));
+//});
 
 module.exports = router;
